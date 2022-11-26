da1bea20936f4dbb0450a65bcfb6b01974e66568
LilaSarfson <lilamandarina@hotmail.com>
2022-11-26 12:29:40 +0100

    retoques

--- src/components/CardProject.js
+++ src/components/CardProject.js
@@ -1,14 +1,14 @@
 // import Est from '../assets/Est-03.svg'
 export default function CardProject () {
     return(
-        <>
         <div>
-            <div className="w-5/6 h-3/6 flex flex-row justify-start border-4 rounded-bl-full rounded-tl-full border-dashed border-black p-8 border-black bg-white m-auto">
+            <div className="w-5/6 h-3/6 flex flex-row items-center gap-4 border-4 rounded-bl-full rounded-tl-full border-solid border-black p-8 border-black bg-white m-auto">
+                <div className='bg-verde rounded-full border-full w-10 h-10 self-center'></div>
                 <h3 className="font-poke">Cine de Barrio</h3>
-                <div className='bg-verde rounded-full border-full w-5 h-5'></div>
-                <div className='bg-rojo rounded-full border-full w-5 h-5'></div>
             </div>
+            {/* <div>
+            <p className=" w-full rotate-90 font-poke">></p>
+            </div> */}
         </div>
-        </>
     )
 }
