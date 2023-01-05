4b38d747a5818a036df4e73c28d35e7b1b99630b
Lila Sarfson <lilamandarina@hotmail..com>
2023-01-05 13:06:09 +0100

    contact ok

--- src/pages/ContactView.js
+++ src/pages/ContactView.js
@@ -1,8 +1,15 @@
+import Hello from '../assets/hello.png'
 export default function ContactView (){
     return(
-        <section id='contact' className="m-auto w-4/6 h-full">
-            <h2 className='font-pompi font-bold underline decoration-verde text-5xl'>Get in touch!</h2>
-            <h3 className="font-mono">Get in touch!</h3>
+        <section id='contact' className="m-auto w-4/6 h-full flex flex-col justify-center">
+            {/* <h2 className='font-pompi font-bold underline decoration-verde text-5xl'>Contact.</h2> */}
+            <div className='flex flex-row justify-center self-center gap-8 items-center'>
+                <h3 className="font-mono font-bold uppercase xl:text-6xl xl:h-1/3 text-center text-verde">Say hello!</h3>
+                <img className='w-2/12' src={Hello}/>
+            </div>
+            <address>
+            <p className='font-mono text-center hover:underline decoration-wavy decoration-verde'><a href='mailto:lilamandarina@hotmail.com' className='not-italic'>lilamandarina@hotmail.com →</a></p>
+            </address>
         </section>    
     )
 }
