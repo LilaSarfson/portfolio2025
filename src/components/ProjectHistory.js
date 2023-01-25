e9362f3cd5bf55631bbc3734c9a649b76c0da5f6
LilaSarfson <lilamandarina@hotmail.com>
2023-01-25 15:35:34 +0100

    responsive ok

--- src/components/ProjectHistory.js
+++ src/components/ProjectHistory.js
@@ -4,9 +4,9 @@ export default function ProojectHistory (props){
         <div className="font-mono bg-amarillOscuro border-solid border-2 border-black p-6 flex flex-col justify-around">
                 <div className='flex flex-row gap-4 p-2'>
                     <img className='w-2/12' src={article}/>
-                    <h3 className="font-bold self-center">{props.titulo}</h3>
+                    <h3 className="font-bold self-center text-sm sm:text-base">{props.titulo}</h3>
                 </div>
-                <p className='text-start p-2'>{props.description}</p>
+                <p className='text-start p-2 text-sm sm:text-base'>{props.description}</p>
                 <button className="bg-azul font-bold w-1/3 self-center drop-shadow-md font-pompi"><a href='ww.google.es'>PDF</a></button>
         </div>
     )
--- src/pages/HomeView.js
+++ src/pages/HomeView.js
@@ -3,7 +3,7 @@ import ListChoice from "../components/ListChoice";
 import perfil from '../assets/perfil1910.svg'
 export default function HomeView (){
    return( 
-   <section className="w-3/6  m-auto h-full mb-28 sm:mb-0">
+   <section className="w-5/6 sm:w-3/6  m-auto h-full mb-28 sm:mb-0">
         <div className="flex flex-col items-center h-full gap-24">
             <div className="flex flex-col justify-center w-full h-4/6 items-center mt-8 font-narko">
                 <ProfileApp src={perfil}/>
