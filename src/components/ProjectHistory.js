d104be6c3b758b633fdaf44aa502d0e9e1ae1092
LilaSarfson <lilamandarina@hotmail.com>
2023-02-19 18:09:53 +0100

    ok

--- src/components/ProjectHistory.js
+++ src/components/ProjectHistory.js
@@ -3,7 +3,7 @@ export default function ProojectHistory (props){
     return(   
         <div className="font-rale border-solid border-2 border-black flex flex-col justify-btweeen max-h-100">
                 <div className='flex flex-row gap-4 p-2 bg-azul border-solid border-b-2 border-black max-h-20'>
-                    <img className='w-2/12' src={`Articles/${props.img}`}/>
+                    <img className='w-2/12 md:w-1/12 lg:w-2/12' src={`Articles/${props.img}`}/>
                     <h3 className="font-bold font-narko self-center text-2xl ">{props.titulo}</h3>
                 </div>
                 <div className='p-4 bg-oscuro'>
