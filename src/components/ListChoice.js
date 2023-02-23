2ed6396f4dac268378c3be4ed85f63e6858f5f92
LilaSarfson <lilamandarina@hotmail.com>
2023-02-23 20:58:31 +0100

    otro comit

--- src/components/ListChoice.js
+++ src/components/ListChoice.js
@@ -7,9 +7,9 @@ const [historyon, setHistoryOn] = useState(false)
      }
 
     return(
-            <ul className="flex flex-col sm:flex-row justify-center items-center m-auto h-1/3 w-2/3 gap-1 sm:gap-2 text-xl sm:text-2xl 2xl:text-3xl font-rale">
-                <li className=" hover:animate-pulse border-solid border-2 border-rojo  text-rojo p-2"><a href="#developer">Developing</a></li>
-                <li onClick={()=>props.displayToParent(displayOn())} className="hover:animate-pulse  border-solid border-2 border-black p-2"><a href='#historian' > History</a> </li>
+            <ul className="flex flex-col sm:flex-row justify-center items-center m-auto  w-2/3 gap-3 sm:gap-2 text-lg sm:text-2xl 2xl:text-3xl font-rale">
+                <li className=" hover:animate-bounce border-solid border-2 border-rojo  text-rojo p-2"><a href="#developer">Developing</a></li>
+                <li onClick={()=>props.displayToParent(displayOn())} className="hover:animate-bounce border-solid border-2 border-black p-2"><a href='#historian' > History</a> </li>
             </ul>
     )
 }
--- src/pages/HomeView.js
+++ src/pages/HomeView.js
@@ -4,7 +4,7 @@ import perfil from '../assets/perfil1910.svg'
 export default function HomeView (props){
    return( 
    <section className="w-5/6 sm:h-full sm:w-4/6 2xl:w-3/6 m-auto sm:py-24">
-        <div className="flex flex-col items-center h-full sm:gap-18">
+        <div className="flex flex-col items-center h-full gap-8 sm:gap-18">
             <div className="flex flex-col justify-center w-full h-4/6 items-center mt-4 font-narko">
                 <ProfileApp src={perfil}/>
             </div>
