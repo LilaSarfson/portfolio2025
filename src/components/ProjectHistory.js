a0d27f74635395e344562d811ebcd0e08299b9d0
Lila Sarfson <lilamandarina@hotmail..com>
2023-03-21 22:30:49 +0100

    update test

--- src/components/ProjectHistory.js
+++ src/components/ProjectHistory.js
@@ -3,8 +3,7 @@ export default function ProojectHistory (props){
     return(   
         <div className="font-rale border-solid border-2 border-black flex flex-col justify-btweeen ">
                 <div className='flex flex-row gap-4 p-2 bg-azul border-solid border-b-2 border-black max-h-20'>
-                    <img className='w-16'  src={`./
-portfoliowebLila/Articles/${props.img}`}/>
+                    <img className='w-16'  src={`/portfoliowebLila/Articles/${props.img}`}/>
                     <h3 className="font-bold font-narko self-center text-2xl 2xl:text-3xl  ">{props.titulo}</h3>
                 </div>
                 <div className='p-4 bg-oscuro'>
