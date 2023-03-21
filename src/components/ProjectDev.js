953ab31bf985a1fdfd8d16431b6d96e475788419
Lila Sarfson <lilamandarina@hotmail..com>
2023-03-21 22:40:57 +0100

    update test

--- src/components/ProjectDev.js
+++ src/components/ProjectDev.js
@@ -5,8 +5,7 @@ export default function ProjectDev (props){
     return(
         <>
         <div className={`flex ${props.direction} ${props.gradient} flex-col h-6/12 gap-4 p-4 w-full m-auto drop-shadow-md`}>
-            <img className='w-full xl:w-3/6 h-1/3  brightness-50 hover:brightness-100' src={`./
-portfoliowebLila/projectsimg/${props.imagen}`} alt={props.alt}></img>    
+            <img className='w-full xl:w-3/6 h-1/3  brightness-50 hover:brightness-100' src={`./portfoliowebLila/${props.imagen}`} alt={props.alt}></img>    
             <div className="font-rale text-end w-full xl:w-3/6 h-1/3 self-start xl:text-md">
                 <LinkProj
                 title={props.title}
